75339cd82b19376265d7a654bdcfa85982e4ef9f
Ivan Moore <ivan@teamoptimization.com>
2013-06-16 20:49:52 +0100

    oops - committed wip code - replaced with "hello world" code

--- src/cion_clojure/core.clj
+++ src/cion_clojure/core.clj
@@ -1,5 +1,12 @@
 (ns cion-clojure.core
   (:gen-class))
 
-(client/post "http://ivan:password@localhost:8080/httpAuth/app/rest/projects/"
-	{:body "bobbyboo"
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+  (println "Hello, World!"))
+
+;;(client/post "http://ivan:password@localhost:8080/httpAuth/app/rest/projects/"
+;;	{:body "bobbyboo"
