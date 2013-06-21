05137152d322aaa662d545c45a38eda59dcb799e
robknows <robmoore121@hotmail.co.uk>
2013-06-21 17:36:59 +0100

    Removed superfluous things in core.clj and changed the formatting slightly so that the args for the main function are on the same line.*

--- src/cion_clojure/core.clj
+++ src/cion_clojure/core.clj
@@ -12,10 +12,7 @@
 (defn get-project-names []
 	(parse-str (:body (client/get "http://ivan:password@localhost:8080/httpAuth/app/rest/projects/"))))
   
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
+(defn -main [& args]
   (alter-var-root #'*read-eval* (constantly false))
   (println "Hello, World!")
   (get-project-names))
