b8a2c8843b317dab3a4da1fad39fb288b48a4669
Ivan Moore <ivan@teamoptimization.com>
2013-09-08 14:38:40 +0100

    tabs vs spaces

--- src/cion_clojure/core.clj
+++ src/cion_clojure/core.clj
@@ -1,17 +1,16 @@
 (ns cion-clojure.core
-  (:gen-class)
-  (:require [clj-http.client :as client])
-  (:require [clojure.data.json :as json])
-  (:use [clojure.data.xml], [clojure.tools.cli :only [cli]]))
+	(:gen-class)
+	(:require [clj-http.client :as client])
+	(:require [clojure.data.json :as json])
+	(:use [clojure.data.xml], [clojure.tools.cli :only [cli]]))
 
 (defn create-project [project-name ci-server-url]
-  (client/post (str ci-server-url "/httpAuth/app/rest/projects")
-  {:body project-name
-   :content-type "text/plain"
-   :accept :xml}))
-   
-(defn create-projects [project-names ci-server-url]
-	(map create-project project-names ci-server-url))
+	(client/post (str ci-server-url "/httpAuth/app/rest/projects")
+		{	
+			:body project-name
+			:content-type "text/plain"
+			:accept :xml
+		}))
 
 (defn extract-name [node]
 	(:name (:attrs node)))
@@ -26,22 +25,23 @@
 	(str "\"" s "\""))
 
 (defn -main [& args]
-  (alter-var-root #'*read-eval* (constantly false))
-  (def command-line-options
-	(cli args 
-	["-u" "--url" "CI server api url" :default ""]
-	["-f" "--file" "Setup CI server using specified file" :default ""]))
-  (if (= args nil) 
-	(print (nth command-line-options 2))
-	(do
-		(def command-line-values
-			(first command-line-options))
-		(if (= (:file command-line-values) "")
-			(do
-				(def project-names (get-project-names (:url command-line-values)))
-				(defn project-as-json [project-name]
-					(str "{	\"project\": { \"name\": " (quoted project-name) "}}"))
-				(println "{ \"projects\" : [" (clojure.string/join ",\n " (map project-as-json project-names)) "] }"))
-			(do
-				(def projects (:projects (json/read-str (slurp (:file command-line-values)) :key-fn keyword)))
-				(println projects))))))
+	(alter-var-root #'*read-eval* (constantly false))
+	(def command-line-options
+		(cli args 
+			["-u" "--url" "CI server api url" :default ""]
+			["-f" "--file" "Setup CI server using specified file" :default ""]))
+	(if (= args nil)
+		(print (nth command-line-options 2))
+		(do
+			(def command-line-values (first command-line-options))
+			(def ci-server-url (:url command-line-values))
+			(if (= (:file command-line-values) "")
+				(do
+					(def project-names (get-project-names ci-server-url))
+					(defn project-as-json [project-name]
+						(str "{	\"project\": { \"name\": " (quoted project-name) "}}"))
+					(println "{ \"projects\" : [" (clojure.string/join ",\n " (map project-as-json project-names)) "] }"))
+				(do
+					(def projects (map :project (:projects (json/read-str (slurp (:file command-line-values)) :key-fn keyword))))
+					(def project-names (map :name projects))
+					(print project-names))))))
